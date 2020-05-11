366e4eddaf3b10f4bfe982dc99fdd9a563d196ea
Clinton Totten <cjtotten@localhost.localdomain>
2020-05-11 17:10:12 -0400

    Adding iterator module

---
+++ src/Iterators.py
@@ -0,0 +1,27 @@
+'''
+Created on May 11, 2020
+
+@author: cjtotten
+'''
+class MyNumbers(object):
+            
+    def __iter__(self):
+        self.a = 1
+        return self
+    
+    def __next__(self):
+        if self.a <= 20:
+          x = self.a
+          self.a += 1
+          return x
+        else:
+          raise StopIteration
+
+myclass = MyNumbers()
+myiter = iter(myclass)
+
+for x in myiter:
+  print(x)
+
+    
+        
